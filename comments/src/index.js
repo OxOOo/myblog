b1373f6dfcd0db253ddb8a86d655a6263652b3ea
yuguotianqing <believe.chenyu@gmail.com>
2017-05-25 16:38:14 +0800

    listening localhost:4000

--- comments/src/index.js
+++ comments/src/index.js
@@ -1,5 +1,6 @@
 
 const app = require('./server');
+const http = require('http');
 
-app.listen(4000);
+http.createServer(app.callback()).listen(4000, '127.0.0.1');
 console.log('listening on http://localhost:4000');
